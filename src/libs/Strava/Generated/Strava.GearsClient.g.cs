28697bca8c8ee908810a1133b434bd386bec69f5
Konstantin S. <havendv@gmail.com>
2026-08-14 19:42:59 +0400

    feat: Updated OpenAPI spec (#30)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Strava/Generated/Strava.GearsClient.g.cs
+++ src/libs/Strava/Generated/Strava.GearsClient.g.cs
@@ -33,10 +33,17 @@ namespace Strava
 
         /// <inheritdoc/>
         public global::Strava.AutoSDKClientOptions Options { get; }
+
+        internal global::System.Lazy<global::System.Text.Json.Serialization.JsonSerializerContext> JsonSerializerContextProvider { get; set; } = new(() => global::Strava.SourceGenerationContext.Default);
+
         /// <summary>
         /// 
         /// </summary>
-        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext { get; set; } = global::Strava.SourceGenerationContext.Default;
+        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext
+        {
+            get => JsonSerializerContextProvider.Value;
+            set => JsonSerializerContextProvider = new(() => value);
+        }
 
 
         /// <summary>
